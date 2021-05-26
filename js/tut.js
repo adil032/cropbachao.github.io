40a9ef38a4ee0d869dc25f0a001ebf97d9e9b7b5
adil032 <66691957+adil032@users.noreply.github.com>
2021-05-26 16:44:47 +0530

    cropbachao.com

---
+++ js/tut.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    document.getElementById("demo").style.left = "0";
+    document.getElementById("gaib").style.display = "block";
+    document.getElementById("gaib1").style.display = "none";
+  }
+  function my() {
+    document.getElementById("demo").style.left = "-100%";
+    document.getElementById("gaib1").style.display = "block";
+    document.getElementById("gaib").style.display = "none";
+  }
